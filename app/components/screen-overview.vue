38e5d5905b2c3521caed0a80d725306f1f100796
Hay Kranen <huskyr@gmail.com>
2018-06-02 13:45:38 +0200

    Adding footer

--- app/components/screen-overview.vue
+++ app/components/screen-overview.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class="section content container">
-        <h1>FitTimer</h1>
+    <div>
+        <div class="section content container">
+            <h1>FitTimer</h1>
 
-        <p>These workouts are available:</p>
+            <p>These workouts are available:</p>
 
-        <ul>
-            <li v-for="(workout, index) in workouts"
-                v-bind:key="index">
-                <router-link v-bind:to="workout.to">
-                    {{workout.name}}
-                </router-link>
-            </li>
-        </ul>
+            <ul>
+                <li v-for="(workout, index) in workouts"
+                    v-bind:key="index">
+                    <router-link v-bind:to="workout.to">
+                        {{workout.name}}
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+
+        <footer class="footer">
+            By <a href="https://www.haykranen.nl/">Hay Kranen</a>.
+            Code on <a href="https://github.com/hay/fittimer">Github</a>.
+        </footer>
     </div>
 </template>
 
