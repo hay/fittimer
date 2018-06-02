0fbf48b473c975d97ac48871b3b263f077d06d70
Hay Kranen <huskyr@gmail.com>
2018-06-02 13:43:04 +0200

    Fetch doesn't send credentials by default

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,9 @@
 import view from './view.js'
 
 async function main() {
-    const req = await fetch('data/data.json');
+    const req = await fetch('data/data.json', {
+        credentials: 'include'
+    });
     const data = await req.json();
     window.__view__ = view(data);
 }
